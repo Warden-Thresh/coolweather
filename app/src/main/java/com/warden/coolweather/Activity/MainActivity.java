e71a3ecbcf3c5a00c95decbfc7ad8eb2bd46d43c
YangJiaYing <1570263943@qq.com>
2017-04-09 23:09:56 +0800

    加入创建数据库和表的配置

--- app/src/main/java/com/warden/coolweather/Activity/MainActivity.java
+++ app/src/main/java/com/warden/coolweather/Activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.warden.coolweather;
+package com.warden.coolweather.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.warden.coolweather.R;
 
 public class MainActivity extends AppCompatActivity {
 
