f28c2cb9a824724b234edd579f3111def4eb9bc1
YangJiaYing <1570263943@qq.com>
2017-04-11 23:12:32 +0800

    修正布局文件wrap_content错误

--- app/src/main/java/com/warden/coolweather/util/Utility.java
+++ app/src/main/java/com/warden/coolweather/util/Utility.java
@@ -3,9 +3,11 @@ package com.warden.coolweather.util;
 import android.text.TextUtils;
 import android.util.Log;
 
+import com.google.gson.Gson;
 import com.warden.coolweather.db.City;
 import com.warden.coolweather.db.County;
 import com.warden.coolweather.db.Province;
+import com.warden.coolweather.gson.Weather;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -85,4 +87,18 @@ public class Utility {
         return false;
     }
 
+    /**
+     * 将返回的JSON数据解析成Weather实体类
+     */
+    public static Weather handleWeatherResponse(String response){
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent,Weather.class);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
